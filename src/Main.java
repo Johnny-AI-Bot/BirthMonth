4918a78784e01463041b12ed5c3ed4b5d07279d2
jatur <turayjj@mail.uc.edu>
2024-09-30 11:36:52 -0400

    Init Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int birthMonth = 0;
+        String trash = "";
+
+        System.out.print("Enter the month (1-12): ");
+        if (in.hasNextInt())
+
+            birthMonth = in.nextInt();
+        in.nextLine(); // clear the buffer
+        if(birthMonth >= 1 && birthMonth <= 12)
+        {
+            System.out.print("You said your birth month is " + birthMonth);
+
+        }
+        else
+        {
+
+        }
+
+        trash = in.nextLine();
+        System.out.print("You entered " + trash + " which is not a valid number ");
+        System.out.println("Run the program again with correct input!");
     }
 }
